cac9a59f6fce770e0674294e494f930ae991d4b5
tarunkumawat1004 <49490869+tarunkumawat1004@users.noreply.github.com>
2022-08-27 19:35:01 +0530

    Update streamlit_app.py
    Reading text file in Python Pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,7 @@ streamlit.text('🥗 Kale, Spanich and Rocket Smoothie !')
 streamlit.text('🐔 Hard-Boiled free range Eggs !')
 streamlit.text('🥑🍞 Avacado Toast !')
 streamlit.header('🍌🥭Build Your Own Fruit Smoothie🥝🍇')
+
+import pandas as pd
+my_fruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
