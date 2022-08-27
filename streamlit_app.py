83785bb2bb974265478f8ac87f6da98dc95c9029
tarunkumawat1004 <49490869+tarunkumawat1004@users.noreply.github.com>
2022-08-27 19:19:31 +0530

    Update streamlit_app.py
    New Menu Added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents new healthy diner')
+streamlit.header('Breaking News')
+streamlit.text('Omega 3 & Blueberry Oatmeal !')
+streamlit.text('Kale, Spanich and Rocket Smoothie !')
+streamlit.text('Hard-Boiled free range Eggs !')
