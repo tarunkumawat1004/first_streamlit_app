c3a2a7018563473dbe773a78b248675387a87e8d
tarunkumawat1004 <49490869+tarunkumawat1004@users.noreply.github.com>
2022-08-28 22:58:27 +0530

    Update streamlit_app.py
    Second choice added

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,10 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
+streamlit.header("Fruityvice Fruit Second Advice!")
+add_my_fruit = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', add_my_fruit)
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
